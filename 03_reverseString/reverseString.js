60d2147225e824744d0ba78d4bc29659e234b0db
abir364 <abirh364@gmail.com>
2022-08-09 05:46:29 +0600

    Completed the reverseString method passed all tests

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,11 @@
-const reverseString = function() {
-
+const reverseString = function(s) {
+    let num = s.length-1;
+    let r = '';
+    while (num >= 0) {
+        r += s.charAt(num);
+        num--;
+    }
+    return r;
 };
 
 // Do not edit below this line
