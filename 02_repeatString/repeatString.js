56b3f1181ea09c7edeb15e24c2117e99a3d7ba43
abir364 <abirh364@gmail.com>
2022-08-09 05:29:47 +0600

    Completed task 2 passed all test for repeatString

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,13 @@
-const repeatString = function() {
-
+const repeatString = function(s, num) {
+    let r = '';
+    if (num<0) {
+        r = 'ERROR';
+    } else {
+        for (let i = 1; i <= num; i++) {
+            r += s;
+        }
+    }
+    return r;
 };
 
 // Do not edit below this line
