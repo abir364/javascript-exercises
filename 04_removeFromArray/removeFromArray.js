d96a1c84ff1028fcc8d50e3fad1fdc4dfde08686
abir364 <abirh364@gmail.com>
2022-08-10 05:31:29 +0600

    Completed task 4 test successful

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,13 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(...arg) {
+    const array = arg[0];
+    const values = arg.slice(1);
+    let newArray = [];
+    for (let i = 0; i < array.length; i++) {
+        if (!values.includes(array[i])) {
+            newArray.push(array[i]);
+        }
+    }
+    return newArray;
 };
 
 // Do not edit below this line
