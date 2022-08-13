3708b0dd2084c0c6d408890cd77e1d52e635ab0c
abir364 <abirh364@gmail.com>
2022-08-14 05:56:11 +0600

    Completed tast 5 sumAll method made to pass tests

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,22 @@
-const sumAll = function() {
-
+const sumAll = function(a,b) {
+    let sum = 0;
+    let start = 0;
+    let end = 0;
+    if (a>=b) {
+        start = b;
+        end = a;
+    } else {
+        start = a;
+        end = b;
+    }
+    if (a<0 || b<0 || !Number.isInteger(a) || !Number.isInteger(b)) {
+        return 'ERROR';
+    } else {
+        for (let i = start; i <= end; i++) {
+            sum += i;
+        }
+    }
+    return sum;
 };
 
 // Do not edit below this line
