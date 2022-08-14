67eabdd2a3c632eab0145097449a2780ab4a2ae3
abir364 <abirh364@gmail.com>
2022-08-14 06:12:39 +0600

    Completed task 6 leapYears methods find leap years accuratedly

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,5 +1,9 @@
-const leapYears = function() {
-
+const leapYears = function(year) {
+    let r = false;
+    if (year%4==0 && (year%100!==0 || year%400==0)) {
+        r = true;
+    }
+    return r;
 };
 
 // Do not edit below this line
