0a690a9939db4d4ce4fff5921515c20e8f349790
abir364 <abirh364@gmail.com>
2022-08-16 05:56:03 +0600

    Completed the calculator task all calculator functions passed the tests

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,49 @@
-const add = function() {
-	
+const add = function(a,b) {
+	return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a,b) {
+	if (a>b) {
+    return a - b;
+  } else {
+    return b - a;
+  }
 };
 
-const sum = function() {
-	
+const sum = function(a) {
+  let sum = 0;
+	for (let i = 0; i < a.length; i++) {
+    sum += a[i];
+  }
+  return sum;
 };
 
-const multiply = function() {
-
+const multiply = function(a) {
+  let result = 1;
+  for (let i = 0; i < a.length; i++) {
+    result *= a[i];
+  }
+  return result;
 };
 
-const power = function() {
-	
+const power = function(a,b) {
+  let res = a;
+	for (let i = 2; i <= b; i++) {
+    res *= a;
+  }
+  return res;
 };
 
-const factorial = function() {
-	
+const factorial = function(a) {
+  if (a===0) {
+    return 1;
+  } else {
+    let res = 1;
+    for (let i = a; i >= 1; i--) {
+      res *= i
+    }
+    return res;
+  }
 };
 
 // Do not edit below this line
