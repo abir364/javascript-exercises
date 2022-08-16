fb2642d3b4b436ad26e26a8957e5d943dcad6477
abir364 <abirh364@gmail.com>
2022-08-16 06:11:58 +0600

    Completed task 09 palindrome passed all tests

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,11 @@
-const palindromes = function () {
-
+const palindromes = function (s) {
+    let word = s.replace(/[^a-zA-Z]/g,"").toLowerCase();
+    for (let i = 0, j = word.length-1; i < word.length/2, j >= word.length/2+1; i++,j--) {
+        if (word[i]!==word[j]) {
+            return false;
+        }
+    }
+    return true;
 };
 
 // Do not edit below this line
