d5f8d7f716ffa14fd0a37ee5ef16dc8ef44182a5
abir364 <abirh364@gmail.com>
2022-08-19 04:42:39 +0600

    Finished task 12 all tests passed

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,6 +1,17 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(p) {
+    return p.reduce((pre_item, cur_item)=>{
+        let cur_age = getAge(cur_item.yearOfDeath, cur_item.yearOfBirth);
+        let pre_age = getAge(pre_item.yearOfDeath, pre_item.yearOfBirth);
+        return pre_age < cur_age ? cur_item : pre_item;
+    })
 };
+
+function getAge(death, birth) {
+    if(!death){
+        death = new Date().getFullYear();
+    }
+    return death-birth;
+}
 
 // Do not edit below this line
 module.exports = findTheOldest;
