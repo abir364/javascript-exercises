5498e31a663ff77d2188151cfb65a187debbed0d
abir364 <abirh364@gmail.com>
2022-08-20 06:04:37 +0600

    Written basic implementation for the caesar cypher method passed 2 tests

--- 13_caesar/caesar.js
+++ 13_caesar/caesar.js
@@ -1,5 +1,14 @@
-const caesar = function() {
-
+const caesar = function(text, num) {
+    let t = '';
+    for (let i = 0; i < text.length; i++) {
+        let cur = text.charCodeAt(i);
+        if ( cur>=65 && cur<=90 || cur>=97 && cur<=122) {
+            t += String.fromCharCode(cur + num);
+        } else {
+            t += String.fromCharCode(cur);
+        }
+    }
+    return t;
 };
 
 // Do not edit below this line
