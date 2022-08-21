8247cdb26bfd2d6d3a0052654db7ee13f63e320a
abir364 <abirh364@gmail.com>
2022-08-22 04:06:34 +0600

    Completed caesar cypher method passed all tests

--- 13_caesar/caesar.js
+++ 13_caesar/caesar.js
@@ -1,9 +1,25 @@
-const caesar = function(text, num) {
+const caesar = function(text, n) {
+    let num = 0;
+    if (n>26) {
+        num = n % 26;
+    } else {
+        num = n;
+    }
     let t = '';
     for (let i = 0; i < text.length; i++) {
         let cur = text.charCodeAt(i);
         if ( cur>=65 && cur<=90 || cur>=97 && cur<=122) {
-            t += String.fromCharCode(cur + num);
+            let code = cur + num;
+            if (code>90 && cur<=90) {
+                code = code - 90 + 65 -1;
+            } else if (code>122 && cur<=122) {
+                code = code - 122 +97 - 1;
+            } else if  (code<65) {
+                code = 90 - 65 + code + 1;
+            } else if (code<97 && cur>=97) {
+                code = 122 - 97 + code + 1;
+            }
+            t += String.fromCharCode(code);
         } else {
             t += String.fromCharCode(cur);
         }
